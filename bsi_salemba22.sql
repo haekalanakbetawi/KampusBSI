14b5fb2490a3c5c2b5883624d5d0669157a3847a
haekalanakbetawi <37955880+haekalanakbetawi@users.noreply.github.com>
2018-04-02 21:49:25 +0700

    Add files via upload

---
+++ bsi_salemba22.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 02 Apr 2018 pada 16.48
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bsi_salemba22`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dosen`
+--
+-- Error reading structure for table bsi_salemba22.dosen: #1932 - Table 'bsi_salemba22.dosen' doesn't exist in engine
+-- Error reading data for table bsi_salemba22.dosen: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bsi_salemba22`.`dosen`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwal`
+--
+-- Error reading structure for table bsi_salemba22.jadwal: #1932 - Table 'bsi_salemba22.jadwal' doesn't exist in engine
+-- Error reading data for table bsi_salemba22.jadwal: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bsi_salemba22`.`jadwal`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jurusan`
+--
+-- Error reading structure for table bsi_salemba22.jurusan: #1932 - Table 'bsi_salemba22.jurusan' doesn't exist in engine
+-- Error reading data for table bsi_salemba22.jurusan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bsi_salemba22`.`jurusan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+-- Error reading structure for table bsi_salemba22.mahasiswa: #1932 - Table 'bsi_salemba22.mahasiswa' doesn't exist in engine
+-- Error reading data for table bsi_salemba22.mahasiswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bsi_salemba22`.`mahasiswa`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `matkul`
+--
+-- Error reading structure for table bsi_salemba22.matkul: #1932 - Table 'bsi_salemba22.matkul' doesn't exist in engine
+-- Error reading data for table bsi_salemba22.matkul: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bsi_salemba22`.`matkul`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ruang`
+--
+-- Error reading structure for table bsi_salemba22.ruang: #1932 - Table 'bsi_salemba22.ruang' doesn't exist in engine
+-- Error reading data for table bsi_salemba22.ruang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bsi_salemba22`.`ruang`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
